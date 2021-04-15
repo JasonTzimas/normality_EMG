ed3e00d721d6d173052c4ccf5493716e40a96779
Iason <jason.tzimas@gmail.com>
2021-04-15 20:40:51 +0300

    plt.close() added

--- functions/normality_function.py
+++ functions/normality_function.py
@@ -26,7 +26,7 @@ def normality_fun(emg1, emg2, quantiles):
     axs[1].set_ylabel("raw EMG (mV)", fontsize=20)
     fig.suptitle("Raw and Normalized EMG from biceps and triceps", y=1.08, fontsize=22)
     fig.tight_layout()
-    plt.show()
+    plt.close()
 
     # Plot histogramm and best fit Gaussian
     plt.rcParams["figure.figsize"] = (18,5)
@@ -46,7 +46,7 @@ def normality_fun(emg1, emg2, quantiles):
     axs[1].plot(bins, y, linewidth=4)
     fig.suptitle("EMG biceps and triceps histogram vs best fit Gaussian", y=1.08, fontsize=22)
     fig.tight_layout()
-    plt.show()
+    plt.close()
 
     # Plot qq-plot
     fig, axs = plt.subplots(1,2)
@@ -54,7 +54,7 @@ def normality_fun(emg1, emg2, quantiles):
     qqplot(emg2, line='s', ax=axs[1])
     fig.suptitle("QQ-Plots for normality checking", y=1.08, fontsize=22)
     fig.tight_layout()
-    plt.show()
+    plt.close()
 
     # D' agostino normality test
     stat, p = normaltest(emg1)
